3586bcbb3ecd0e0b6a437145e58d1b37ec7948cb
MishraShardendu22 <shardendumishrabrave@gmail.com>
2025-08-26 21:09:06 +0530

    fix: sidebar fixed
    Signed-off-by: MishraShardendu22 <shardendumishrabrave@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -108,7 +108,7 @@ export default function HomePage() {
           <LazyExperienceSection />
         </section>
 
-        <section id="experience">
+        <section id="volunteer">
           <LazyVExperienceSection />
         </section>
 
