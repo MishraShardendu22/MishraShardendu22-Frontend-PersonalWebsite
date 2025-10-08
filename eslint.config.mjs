1f4d203c680d3b5f2091ae2c6b0d9b4cc416a50d
MishraShardendu22 <shardendumishrabrave@gmail.com>
2025-10-09 01:17:14 +0530

    fix: update ESLint configuration in next.config.ts
    Signed-off-by: MishraShardendu22 <shardendumishrabrave@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
-import { FlatCompat } from '@eslint/eslintrc'
 import path from 'path'
-import { fileURLToPath } from 'url'
 import js from '@eslint/js'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -14,31 +14,31 @@ const compat = new FlatCompat({
 const eslintConfig = [
   {
     ignores: [
-      '.next/**',
       'out/**',
-      'build/**',
       'dist/**',
-      'node_modules/**',
-      '*.config.js',
-      '*.config.mjs',
-      '*.config.ts',
+      '.next/**',
+      'build/**',
       'public/**',
       '.vercel/**',
       'coverage/**',
+      '*.config.js',
+      '*.config.ts',
+      '*.config.mjs',
+      'node_modules/**',
     ],
   },
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
     rules: {
+      '@typescript-eslint/ban-ts-comment': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unsafe-function-type': 'off',
-      '@typescript-eslint/no-empty-object-type': 'off',
-      '@typescript-eslint/no-wrapper-object-types': 'off',
-      '@typescript-eslint/ban-ts-comment': 'off',
       '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
       '@typescript-eslint/no-unused-expressions': 'off',
       '@typescript-eslint/triple-slash-reference': 'off',
+      '@typescript-eslint/no-unsafe-function-type': 'off',
+      '@typescript-eslint/no-wrapper-object-types': 'off',
     },
   },
 ]
